8b93c5f10233d6194fff2f54a29d12dba27cd39c
Leandro Hurtado Salazar <leandhusar@gmail.com>
2021-01-07 17:54:04 -0500

    Here is the Champion Editor (base)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'HeroList';
+  title = 'Champion Build List';
 }
